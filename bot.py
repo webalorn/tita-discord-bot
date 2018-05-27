6604651f631a46d5f0055afdeb953b37ca925e06
Theophane Vallaeys <webalorn@gmail.com>
2018-05-27 15:11:32 +0200

    Improvments

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from discord import Game
 from discord.ext.commands import Bot
 
 BOT_PREFIX = ("!")
-keywords_delay = 1
+keywords_delay = 0.5
 everyone_admin = False
 
 client = Bot(command_prefix=BOT_PREFIX)
@@ -71,14 +71,17 @@ def commandAdmin(*p, **pn):
         return client.command(*p, **pn)(wrapper)
     return decorated
 
+def getRandomSentence(category):
+    if category in config['random'] and len(config['random'][category]):
+        return random.choice(config['random'][category])
+    return "J'en reste sans voix :-*"
+
 ##### COMMANDS #####
 
 # Music
 
-@commandAdmin(name='add-music',
-                brief="Add music",
-                description="Add a music from youtube url",
-                aliases=['add'])
+@client.command(name='add-music', aliases=['add'], pass_context=True,
+                brief="Add music", description="Add a music from youtube url")
 async def addMusic(context, songname, url):
     if not re.match(r'^[a-zA-Z_0-9]+$', songname):
         return await client.say('Un tel nom n\'est pas concevable, humain, même si cela dépasse ton entendement')
@@ -94,17 +97,14 @@ async def addMusic(context, songname, url):
     save_config()
     await client.say("Je dispose maintenant de l'arsenal musical {0} !".format(songname))
 
-@client.command(name='play-music',
-                brief="Play music",
-                description="Play a music added with add-music command",
-                aliases=['play'],
-                pass_context=True)
+@client.command(name='play-music', aliases=['play'], pass_context=True,
+                brief="Play music", description="Play a music added with add-music command")
 async def playMusic(context, songname):
     if not songname in config['music']:
         return await client.say("Stupide humain, cette musique n'existe pas !")
     await playYoutubeMusic(config['music'][songname]['url'], context.message.server.channels)
 
-@commandAdmin(name='stop-music', aliases=['stop'])
+@commandAdmin(name='stop-music', aliases=['stop'], brief="[ADMIN]")
 async def stopMusic(context):
     stopMusicPlayer()
 
@@ -154,6 +154,18 @@ async def addKeyword(keyword, command):
     save_config()
     await client.say('Je serais bientot plus reactif a tout les {0}'.format(keyword))
 
+@client.command(name='list-keywords')
+async def listKeywords():
+    await client.say('Je connais le sens absolu de: {0}'.format(", ".join("**{0}**".format(m) for m in config['keywords'].keys())))
+
+@commandAdmin(name='rm-keyword', brief="[ADMIN]")
+async def removeKeyword(context, keyword):
+    del config['keywords'][keyword]
+    save_config()
+    await client.say('Je retire tout pouvoir a la formule {0}'.format(keyword))
+
+# Aliases
+
 @client.command(name='add-alias', aliases=['alias'])
 async def addAlias(keyword, aliasOf):
     keyword = str(keyword).lower()
@@ -163,6 +175,42 @@ async def addAlias(keyword, aliasOf):
     config['aliases'][keyword] = aliasOf
     save_config()
     await client.say('Ainsi {0} signifira dorénavant {1} pour tous, parceque je le veux'.format(keyword, aliasOf))
+
+@client.command(name='list-aliases')
+async def listAliases():
+    await client.say('Ces mots sont maintenant tout autres: {0}'.format(", ".join(
+        "**{0}**[{1}]".format(m, config['aliases'][m]) for m in config['aliases'].keys()
+    )))
+
+@commandAdmin(name='rm-alias', brief="[ADMIN]")
+async def removeAlias(context, keyword):
+    del config['aliases'][keyword]
+    save_config()
+    await client.say('J\'accorde le droit de retour a sa forme première aux mot {0}'.format(keyword))
+
+# Random sentences
+
+@client.command(name='add-random')
+async def addRandom(category, sentence):
+    if not category in config['random']:
+        config['random'][category] = []
+    config['random'][category].append(sentence)
+    save_config()
+    await client.say('Ainsi, {0} pourra signifier {1}'.format(category, sentence))
+
+@client.command(name='random')
+async def randomSaySenetence(category):
+    await client.say(getRandomSentence(category))
+
+@client.command(name='list-random')
+async def getRandomCategoryList():
+    await client.say('Je suis imprévisible, quand on parle de: {0}'.format(", ".join("**{0}**".format(m) for m in config['random'].keys())))
+
+@commandAdmin(name='rm-random', brief="[ADMIN]")
+async def removeRandomCategory(context, category):
+    del config['random'][category]
+    save_config()
+    await client.say('Finalement, je ne dirait plus rien de {0}'.format(category))
 
 ##### EVENTS #####
 
@@ -195,10 +243,6 @@ async def on_ready():
 async def on_message(message):
     global musicChannel, musicPlayer
 
-    if musicChannel and not musicPlayer:
-        await musicChannel.disconnect()
-        musicChannel = None
-
     msg_text = message.clean_content.lower()
 
     if msg_text[:1] in BOT_PREFIX:
@@ -214,6 +258,9 @@ async def on_message(message):
         if client.user.id in message.raw_mentions:
             await sayItsMe(message)
 
+    if musicChannel and not musicPlayer:
+        await musicChannel.disconnect()
+        musicChannel = None
 
 async def list_servers():
     await client.wait_until_ready()
@@ -235,6 +282,8 @@ finally:
         config['keywords'] = {}
     if not 'aliases' in config:
         config['aliases'] = {}
+    if not 'random' in config:
+        config['random'] = {}
     if not 'admins' in config:
         config['admins'] = ['211191341533757440']
 
